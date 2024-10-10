e95918f917535a8606d9b741e7e5a46d65c35e84
Giacomo Debidda <giacomo@giacomodebidda.com>
2024-10-10 23:34:09 +0200

    setup developer environment

---
+++ dev/user.cljs
@@ -0,0 +1,6 @@
+(ns user
+  "Tools for interactive development with the REPL.
+  This file should not be included in a production build of the application.")
+
+(comment
+  (+ 1 2))
---
+++ devenv.nix
@@ -0,0 +1,46 @@
+{pkgs, ...}: {
+  enterShell = ''
+    versions
+  '';
+
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  env = {
+    # GIT_NOTES_NAMESPACE = "foo"; # custom namespace for git notes
+    # GIT_NOTES_REF = "refs/notes/commits"; # default
+    GIT_NOTES_REF = "refs/notes/foo"; # custom namespace for git notes
+    GREET = "devenv";
+  };
+
+  languages = {
+    clojure.enable = true;
+    nix.enable = true;
+  };
+
+  packages = [
+    pkgs.babashka
+    pkgs.git
+    pkgs.nodejs
+  ];
+
+  pre-commit.hooks = {
+    alejandra.enable = true;
+    cljfmt.enable = true;
+    deadnix.enable = true;
+    shellcheck.enable = true;
+    statix.enable = true;
+  };
+
+  scripts = {
+    versions.exec = ''
+      echo "=== Versions ==="
+      bb --version
+      git --version
+      echo "Node.js $(node --version)"
+      echo "=== === ==="
+    '';
+  };
+}
